9c244e894d8e5cd2735140515a94459e73ebf3ea
Ádrian Gama <adrian.gama.se@gmail.com>
2024-09-11 10:39:28 -0300

    change `get_images.sh` target path

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+TARGET=samples
 
-git clone https://github.com/ddantas/visiongl_images.git images
-cd images
+git clone https://github.com/ddantas/visiongl_images.git $TARGET
+cd $TARGET
 unzip mitosis.zip
 cd ..
 
-echo Sample images downloaded to folder \"./images/\"
+echo Sample images downloaded to folder "./$TARGET/"
