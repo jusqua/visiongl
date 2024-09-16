855c193a771f97347315bbf4bdb0f78bdc8ca4c6
Ádrian Gama <adrian.gama.se@gmail.com>
2024-09-15 22:57:25 -0300

    fix header importing when using OpenCL compiler

--- include/vglClStrEl.h
+++ include/vglClStrEl.h
@@ -1,13 +1,10 @@
-
 #ifndef __VGLCLSTREL_H__
 #define __VGLCLSTREL_H__
 
 #include <visiongl/vglConst.h>
-#include <CL/cl.h>
 
-
-// OpenCL
-#ifdef __OPENCL_VERSION__
+// OpenCL compiler definition
+#if defined(CL_VERSION_1_0)
 typedef struct VglClStrEl{ 
   float data[VGL_ARR_CLSTREL_SIZE];
   int ndim;
@@ -15,10 +12,9 @@ typedef struct VglClStrEl{
   int offset[VGL_ARR_SHAPE_SIZE];
   int size;
 } VglClStrEl;
-#endif
-
-// C++
-#ifdef __OPENCL__
+// CXX compiler definition
+#elif defined(__OPENCL__)
+#include <CL/cl.h>
 typedef struct VglClStrEl{ 
   cl_float data[VGL_ARR_CLSTREL_SIZE];
   cl_int ndim;
